1e74f813d1643f679e8b8fd80ad2bdf65d53c833
Leticia <leticia.amboni@benner.com.br>
2019-08-09 11:36:20 -0300

    login

---
+++ Livraria.Sistema/Livraria.Sistema/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Livraria.Sistema.Models;
+using System.Data.SqlClient;
+
+namespace Livraria.Sistema.Controllers
+{
+    public class LoginController : Controller
+    {
+        SqlConnection conexao = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader dr;
+
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            conexao.ConnectionString = "data source=LETICIA-PC\\SQLEXPRESS; database=BDLIVRARIA; integrated security=SSPI;";
+        }
+        [HttpPost]
+        public ActionResult Verifica(FUNCIONARIOS funcionario)
+        {
+            connectionString();
+            conexao.Open();
+            comando.Connection = conexao;
+            comando.CommandText = "select * from [FUNCIONARIOS] where [USERNAME]='"+funcionario.USERNAME+"' and [SENHA]='"+funcionario.SENHA+"'";
+            dr = comando.ExecuteReader();
+
+            if (dr.Read())
+            {
+                conexao.Close();
+                return View("Create");
+            }
+            else
+            {
+                conexao.Close();
+                return View("Erro");
+            }
+            
+            
+        }
+    }
+}
